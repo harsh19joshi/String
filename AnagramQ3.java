5e06433416d6b9f18501c246d04b7da186ba7a88
harsh <61630862+honey1908@users.noreply.github.com>
2022-08-22 02:04:35 +0530

    Update AnagramQ3.java

--- AnagramQ3.java
+++ AnagramQ3.java
@@ -3,18 +3,36 @@ public class AnagramQ3 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String s1= sc.nextLine();
-        int low=0;
-        int high=s1.length()-1;
-        String s2="";
-        while(low<high){
-            if(s1.charAt(low)==s1.charAt(high)){
-                s2="Anagram";
-            }else{
-                s2="Not Anagram";
+        String s2= sc.nextLine();
+        //String s2="";
+        char temp;
+        char[]ch= s1.toCharArray();
+        for(int i=0;i< ch.length;i++){
+            for(int j=i+1;j< ch.length;j++) {
+                if (ch[i] > ch[j]) {
+                    temp = ch[i];
+                    ch[i] = ch[j];
+                    ch[j] = temp;
+                }
             }
-            low++;
-            high--;
         }
-        System.out.println(s2);
+        char[]ch1= s2.toCharArray();
+        for(int i=0;i< ch1.length;i++){
+            for(int j=i+1;j< ch1.length;j++) {
+                if (ch1[i] > ch1[j]) {
+                    temp = ch1[i];
+                    ch1[i] = ch1[j];
+                    ch1[j] = temp;
+                }
+            }
+        }
+        int n=new String(ch).compareTo(new String(ch1));
+        if(n==0){
+            System.out.println("Anagram");
+        }else {
+            System.out.println("not anagram");
+        }
+
+        //System.out.println(s2);
     }
 }
